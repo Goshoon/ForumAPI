bac69b91da1cc8911f46a01dd52660a6d4e9f303
Goshoon <treskks@gmail.com>
2026-01-12 07:03:04 -0700

    Threads added to database + API

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from datetime import datetime
 
 from database import engine, get_db
-from models import Base, Post
+from models import Base, Thread, Post
 
 # Create FastAPI app
 app = FastAPI(title="Forum API")
@@ -15,6 +15,24 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def health_check():
     return {"status": "ok"}
+
+
+@app.get("/threads")
+def get_posts(db: Session = Depends(get_db)):
+    threads = db.query(Thread).all()
+    return threads
+
+
+@app.post("/threads")
+def create_post(summary: str, db: Session = Depends(get_db)):
+    thread = Thread(
+        summary=summary,
+        time=datetime.utcnow().isoformat()
+    )
+    db.add(thread)
+    db.commit()
+    db.refresh(thread)
+    return thread
 
 
 @app.get("/posts")
--- api/models.py
+++ api/models.py
@@ -1,5 +1,16 @@
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy import Column, Integer, Text, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+class Thread(Base):
+    __tablename__ = "threads"
+
+    id = Column("ID", Integer, primary_key=True, index=True)
+    summary = Column("SUMMARY", Text, nullable=False)
+    time = Column("TIME", Text, nullable=False)
+
+    posts = relationship("Post", back_populates="thread_obj", cascade="all, delete")
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -7,3 +18,12 @@ class Post(Base):
     id = Column("ID", Integer, primary_key=True, index=True)
     comment = Column("COMMENT", Text, nullable=False)
     time = Column("TIME", Text, nullable=False)
+
+    thread = Column(
+        "THREAD",
+        Integer,
+        ForeignKey("threads.ID"),
+        nullable=False
+    )
+
+    thread_obj = relationship("Thread", back_populates="posts")
